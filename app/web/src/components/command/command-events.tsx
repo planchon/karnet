d44cf380eb94a712ec60edbd047ea5feb9f43bc9
planchon <paul@planchon.io>
2025-06-01 15:06:18 +0200

    better events

--- app/web/src/components/command/command-events.tsx
+++ app/web/src/components/command/command-events.tsx
@@ -3,25 +3,19 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle
 } from "../ui/dialog";
 import { commandEventsStore, toggleCreateEvents } from "@/stores/commands";
 import { Input } from "../ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from "../ui/select";
-import {
-  IconBrandApple,
-  IconBrandGoogle,
-  IconBrandMinecraft
-} from "@tabler/icons-react";
-import { ChevronRight } from "lucide-react";
+import { Select, SelectContent, SelectItem, SelectTrigger } from "../ui/select";
+import { IconCalendar, IconChevronRight, IconLabel } from "@tabler/icons-react";
 import { useShortcut } from "@/hooks/useShortcut";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { Button } from "../ui/button";
+import { Switch } from "../ui/switch";
+import { DateTimePicker } from "../super-ui/date-time-picker";
 
 export const CreateEventCommand = () => {
   const open = useSnapshot(commandEventsStore).eventOpen;
@@ -32,33 +26,11 @@ export const CreateEventCommand = () => {
 
   return (
     <Dialog open={open} onOpenChange={toggleCreateEvents}>
-      <DialogContent className="z-[1000] min-w-[700px] p-3">
-        <div className="flex w-full flex-col gap-3">
+      <DialogContent className="z-[1000] min-w-[700px] p-0">
+        <div className="flex w-full flex-col gap-3 p-3 pb-0">
           <div className="flex items-center gap-2">
-            <Select defaultValue="google">
-              <SelectTrigger
-                size="sm"
-                className="shadow-none"
-                showChevron={false}
-              >
-                <SelectValue className="text-xs" placeholder="Google" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="google">
-                  <IconBrandGoogle />
-                  Google
-                </SelectItem>
-                <SelectItem value="outlook">
-                  <IconBrandMinecraft />
-                  Outlook
-                </SelectItem>
-                <SelectItem value="apple">
-                  <IconBrandApple />
-                  Apple
-                </SelectItem>
-              </SelectContent>
-            </Select>
-            <ChevronRight className="size-3" />
+            <CalendarSelect />
+            <IconChevronRight className="size-3" />
             <span className="font-regular text-xs">New event</span>
           </div>
           <div className="flex flex-col gap-3">
@@ -67,15 +39,85 @@ export const CreateEventCommand = () => {
               placeholder="Event name"
               className="text-field-text-default placeholder:text-field-text-placeholder disabled:text-field-text-disabled min-h-[20px] w-full resize-none bg-transparent text-sm outline-none"
             />
-            <input
-              type="text"
-              placeholder="Description"
-              multiple={true}
-              className="text-field-text-default placeholder:text-field-text-placeholder disabled:text-field-text-disabled w-full resize-none bg-transparent text-sm outline-none"
-            />
+          </div>
+          <div className="flex w-full select-none flex-row gap-2 pt-3">
+            <div>
+              <DateTimePicker />
+            </div>
           </div>
         </div>
+        <div className="w-full border-b" />
+        <DialogFooter className="px-3 pb-3">
+          <div className="flex select-none items-center gap-2">
+            <Switch />
+            <span className="text-xs">Create more</span>
+          </div>
+          <Button>Create</Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
+  );
+};
+
+const CalendarSelect = () => {
+  return (
+    <Tooltip delayDuration={800}>
+      <TooltipTrigger autoFocus={false}>
+        <Select>
+          <SelectTrigger
+            showChevron={false}
+            size="sm"
+            className="border shadow-none"
+          >
+            <IconLabel className="text-accent-foreground/60 size-4" />
+            <span className="text-accent-foreground/60 text-xs font-medium">
+              Stratumn
+            </span>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+            <SelectItem value="2">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </TooltipTrigger>
+      <TooltipContent>
+        <span className="text-xs font-medium text-gray-500">
+          Select the calendar
+        </span>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+const Deadline = () => {
+  return (
+    <Tooltip delayDuration={800}>
+      <TooltipTrigger autoFocus={false}>
+        <Select>
+          <SelectTrigger showChevron={false} size="sm" className="w-8 pl-[7px]">
+            <IconCalendar className="text-accent-foreground/60 size-4" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+            <SelectItem value="2">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </TooltipTrigger>
+      <TooltipContent>
+        <span className="text-xs font-medium text-gray-500">Task deadline</span>
+      </TooltipContent>
+    </Tooltip>
   );
 };
--- app/web/src/components/command/command-task.tsx
+++ app/web/src/components/command/command-task.tsx
@@ -4,6 +4,21 @@ import { Button } from "../ui/button";
 import { commandEventsStore, toggleCreateTaskEvents } from "@/stores/commands";
 import { useShortcut } from "@/hooks/useShortcut";
 import { Switch } from "../ui/switch";
+import { Tooltip, TooltipContent } from "../ui/tooltip";
+import { TooltipTrigger } from "../ui/tooltip";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger
+} from "../super-ui/select";
+import {
+  IconAntennaBars3,
+  IconAntennaBars5,
+  IconCalendar,
+  IconChevronRight,
+  IconLabel
+} from "@tabler/icons-react";
 
 export const CreateTaskCommand = () => {
   const open = useSnapshot(commandEventsStore).taskOpen;
@@ -15,27 +30,30 @@ export const CreateTaskCommand = () => {
   return (
     <Dialog open={open} onOpenChange={toggleCreateTaskEvents}>
       <DialogContent className="z-[1000] min-w-[700px] p-0">
-        <div className="flex w-full flex-col gap-3 p-3">
+        <div className="flex w-full flex-col gap-3 p-3 pb-0">
           <div className="flex items-center gap-2">
-            <span className="font-regular text-xs">Create a new task</span>
+            <ProjectSelect />
+            <IconChevronRight className="text-accent-foreground/60 size-4" />
+            <span className="font-regular select-none text-xs">
+              Create a new task
+            </span>
           </div>
           <div className="flex flex-col gap-3">
             <input
               type="text"
-              placeholder="Event name"
+              placeholder="Task name"
               className="text-field-text-default placeholder:text-field-text-placeholder disabled:text-field-text-disabled text-md min-h-[20px] w-full resize-none bg-transparent outline-none"
             />
-            <input
-              type="text"
-              placeholder="Description"
-              multiple={true}
-              className="text-field-text-default placeholder:text-field-text-placeholder disabled:text-field-text-disabled w-full resize-none bg-transparent text-sm outline-none"
-            />
+          </div>
+          <div className="flex w-full select-none flex-row gap-2 pt-3">
+            <TaskPriority />
+            <Deadline />
+            <TaskLabel />
           </div>
         </div>
         <div className="w-full border-b" />
         <DialogFooter className="px-3 pb-3">
-          <div className="flex items-center gap-2">
+          <div className="flex select-none items-center gap-2">
             <Switch />
             <span className="text-xs">Create more</span>
           </div>
@@ -43,5 +61,125 @@ export const CreateTaskCommand = () => {
         </DialogFooter>
       </DialogContent>
     </Dialog>
+  );
+};
+
+const ProjectSelect = () => {
+  return (
+    <Tooltip delayDuration={800}>
+      <TooltipTrigger autoFocus={false}>
+        <Select>
+          <SelectTrigger
+            showChevron={false}
+            size="sm"
+            className="border shadow-none"
+          >
+            <IconLabel className="text-accent-foreground/60 size-4" />
+            <span className="text-accent-foreground/60 text-xs font-medium">
+              Stratumn
+            </span>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+            <SelectItem value="2">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </TooltipTrigger>
+      <TooltipContent>
+        <span className="text-xs font-medium text-gray-500">
+          Select the project
+        </span>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+const TaskLabel = () => {
+  return (
+    <Tooltip delayDuration={800}>
+      <TooltipTrigger autoFocus={false}>
+        <Select>
+          <SelectTrigger showChevron={false} size="sm">
+            <IconLabel className="text-accent-foreground/60 size-4" />
+            <span className="text-accent-foreground/60 text-xs font-medium">
+              Tags
+            </span>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+            <SelectItem value="2">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </TooltipTrigger>
+      <TooltipContent>
+        <span className="text-xs font-medium text-gray-500">Project label</span>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+const TaskPriority = () => {
+  return (
+    <Tooltip delayDuration={800}>
+      <TooltipTrigger autoFocus={false}>
+        <Select>
+          <SelectTrigger showChevron={false} size="sm" className="w-7 pl-1">
+            <IconAntennaBars5 className="text-accent-foreground/60 size-4" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+            <SelectItem value="2">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </TooltipTrigger>
+      <TooltipContent>
+        <span className="text-xs font-medium text-gray-500">Task priority</span>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+const Deadline = () => {
+  return (
+    <Tooltip delayDuration={800}>
+      <TooltipTrigger autoFocus={false}>
+        <Select>
+          <SelectTrigger showChevron={false} size="sm" className="w-7 pl-[5px]">
+            <IconCalendar className="text-accent-foreground/60 size-4" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+            <SelectItem value="2">
+              <IconLabel className="text-accent-foreground/80 size-4" />
+              test
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </TooltipTrigger>
+      <TooltipContent>
+        <span className="text-xs font-medium text-gray-500">Task deadline</span>
+      </TooltipContent>
+    </Tooltip>
   );
 };
