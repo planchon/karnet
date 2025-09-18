ec6d872822572d9b3552b87a3a8680b77a296e92
planchon <paul@planchon.io>
2025-09-19 00:18:40 +0200

    change the default page

--- app/karnet/middleware.ts
+++ app/karnet/middleware.ts
@@ -1,18 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 export default clerkMiddleware(async (auth, req) => {
-	const { userId } = await auth();
-	if (userId && req.nextUrl.pathname === "/") {
-		return NextResponse.redirect(new URL("/document", req.url));
-	}
+    const { userId } = await auth();
+    if (userId && req.nextUrl.pathname === '/') {
+        return NextResponse.redirect(new URL('/chat', req.url));
+    }
 });
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
-		"/(api|trpc)(.*)",
-	],
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
 };
