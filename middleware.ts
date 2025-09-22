4433f9304d429e1a12f2e04471872b9ba2cc4c93
planchon <paul@planchon.io>
2025-09-22 23:35:34 +0200

    restore the chat

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,25 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export default clerkMiddleware(async (auth, req) => {
     const { userId } = await auth();
-    if (!userId && req.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/hello', req.url));
+
+    if (!userId && req.nextUrl.pathname === "/") {
+        return NextResponse.redirect(new URL("/hello", req.url));
     }
-    if (userId && req.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/chat', req.url));
+
+    if (userId && req.nextUrl.pathname === "/") {
+        return NextResponse.redirect(new URL("/chat", req.url));
     }
+
+    return NextResponse.next();
 });
 
 export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
         // Always run for API routes
-        '/(api|trpc)(.*)',
+        "/(api|trpc)(.*)",
     ],
 };
