3dbb7cdf2877a4732f7052ff5d84ea89a0309dca
planchon <paul@planchon.io>
2025-10-04 11:46:51 +0200

    test sentry

--- lib/session.ts
+++ lib/session.ts
@@ -1,39 +1,47 @@
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import * as Sentry from "@sentry/nextjs";
 import { after } from "next/server";
 import { getRedis } from "./cache";
 
 const SESSION_EXPIRATION = 1800;
 
-export const getSession = async () => {
-    const redis = await getRedis();
+export const getSession = () => {
+    return Sentry.startSpan(
+        {
+            name: "getToken",
+        },
+        async () => {
+            const redis = await getRedis();
 
-    const { sessionId } = await auth();
+            const { sessionId } = await auth();
 
-    if (!sessionId) {
-        throw new Error("No session id");
-    }
+            if (!sessionId) {
+                throw new Error("No session id");
+            }
 
-    const jwt = await redis.get(`session:${sessionId}`);
+            const jwt = await redis.get(`session:${sessionId}`);
 
-    if (!jwt) {
-        const tokenRes = await (await clerkClient()).sessions.getToken(sessionId, "convex", SESSION_EXPIRATION);
+            if (!jwt) {
+                const tokenRes = await (await clerkClient()).sessions.getToken(sessionId, "convex", SESSION_EXPIRATION);
 
-        if (!tokenRes) {
-            throw new Error("No token");
+                if (!tokenRes) {
+                    throw new Error("No token");
+                }
+
+                // set the session in redis after the request is complete
+                after(async () => {
+                    await redis.set(`session:${sessionId}`, tokenRes.jwt, {
+                        expiration: {
+                            value: SESSION_EXPIRATION,
+                            type: "EX",
+                        },
+                    });
+                });
+
+                return tokenRes.jwt;
+            }
+
+            return jwt;
         }
-
-        // set the session in redis after the request is complete
-        after(async () => {
-            await redis.set(`session:${sessionId}`, tokenRes.jwt, {
-                expiration: {
-                    value: SESSION_EXPIRATION,
-                    type: "EX",
-                },
-            });
-        });
-
-        return tokenRes.jwt;
-    }
-
-    return jwt;
+    );
 };
---
+++ sentry/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+    dsn: "https://85c164d195e3a6452b517ec4aa7e4194@o4509537188249600.ingest.de.sentry.io/4510130457608272",
+
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+});
