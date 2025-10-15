f97821bf81662f8325a362fa29611249a0ed4f2c
planchon <paul@planchon.io>
2025-10-16 00:27:10 +0200

    routes

--- router/page-wrapper.tsx
+++ router/page-wrapper.tsx
@@ -4,7 +4,11 @@ import { useAuth } from "@clerk/react-router";
 import { Navigate, Outlet } from "react-router";
 
 export const Protected = () => {
-    const { isSignedIn } = useAuth();
+    const { isSignedIn, isLoaded } = useAuth();
+
+    if (!isLoaded) {
+        return null;
+    }
 
     if (!isSignedIn) {
         return <Navigate to="/login" />;
