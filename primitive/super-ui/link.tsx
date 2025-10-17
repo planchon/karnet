42504dc08dd89fa4a25dd0a6ce0489520eb07587
planchon <paul@planchon.io>
2025-10-18 00:45:17 +0200

    fix build and settings

--- primitive/super-ui/link.tsx
+++ primitive/super-ui/link.tsx
@@ -1,32 +1,31 @@
-import { observer } from 'mobx-react';
-import { usePathname } from 'next/navigation';
-import { Link } from 'react-router';
-import { useSettings } from '@/hooks/useStores';
-import { cn } from '@/lib/utils';
-import { SidebarMenuButton, SidebarMenuItem } from '@/primitive/ui/sidebar';
-import { TooltipWrapper } from './tooltip-wrapper';
+import { observer } from "mobx-react";
+import { Link, useLocation } from "react-router";
+import { useSettings } from "@/hooks/useStores";
+import { cn } from "@/lib/utils";
+import { SidebarMenuButton, SidebarMenuItem } from "@/primitive/ui/sidebar";
+import { TooltipWrapper } from "./tooltip-wrapper";
 
 type SuperLinkProps = {
     title: string;
     icon?: React.ComponentType<{ className?: string }>;
     tooltip: {
         title: string;
-        side?: 'top' | 'right' | 'bottom' | 'left';
+        side?: "top" | "right" | "bottom" | "left";
         shortcut?: string[];
     };
 } & React.ComponentProps<typeof Link>;
 
-export const SuperLink = observer(function SuperLink({ title, icon: Icon, tooltip, ...props }: SuperLinkProps) {
+export const SuperLink = observer(function SuperLinkInner({ title, icon: Icon, tooltip, ...props }: SuperLinkProps) {
     const settings = useSettings();
     const linkEnabled = !settings.disableLinks;
 
-    const isActive = usePathname().includes(props.to.toString());
+    const isActive = useLocation().pathname.includes(props.to.toString());
 
     return (
         <Link
             className={cn(
-                'rounded-[4px] text-accent-foreground hover:cursor-pointer',
-                !linkEnabled && 'pointer-events-none select-none'
+                "rounded-[4px] text-accent-foreground hover:cursor-pointer",
+                !linkEnabled && "pointer-events-none select-none"
             )}
             draggable={false}
             key={title}
@@ -37,11 +36,11 @@ export const SuperLink = observer(function SuperLink({ title, icon: Icon, toolti
                 <SidebarMenuItem className="hover:cursor-pointer" key={title} tabIndex={-1}>
                     <SidebarMenuButton className="group/item" tooltip={title}>
                         {Icon && (
-                            <Icon className={cn(isActive ? 'text-accent-foreground' : 'text-accent-foreground/80')} />
+                            <Icon className={cn(isActive ? "text-accent-foreground" : "text-accent-foreground/80")} />
                         )}
-                        <span className="truncate">{title}</span>{' '}
+                        <span className="truncate">{title}</span>{" "}
                     </SidebarMenuButton>
-                </SidebarMenuItem>{' '}
+                </SidebarMenuItem>{" "}
             </TooltipWrapper>
         </Link>
     );
